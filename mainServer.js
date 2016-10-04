3ec29b7909e262f11dac01f25176882a4f954281
Eric M Butler <ericbutler1@Erics-MBP-2.ds.ohnet>
2016-10-04 10:28:16 -0400

    Removed gulp. Everything is in npm scripts now

--- mainServer.js
+++ mainServer.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // development only
 if ('development' == app.get('env')) {
-	app.use(require('errorhandler')());
+  app.use(require('errorhandler')());
 }
 
 /////////////////////
@@ -36,87 +36,87 @@ var bitbucketAuth = 'Basic ZXJpY2J1dGxlcjAwMEBnbWFpbC5jb206MXdkdlpEVCY=';
 var repoUpdateInHours = 12;
 
 // Startup Server!app
-http.createServer(app).listen(app.get('port'), function () {
-	// We are live!
-	console.log('Main Server listening on port ' + app.get('port'));
-	// One time tasks for new server boot
-	runStartupTasks();
-	// Reoccuring tasks
-	setInterval(function () {
-		global.bitbucket_repos = [];
-		global.github_repos = [];
-		getGithubReposInfo();
-		getBitbucketReposInfo();
-		global.repos_updated_last = new moment();
-	}, 1000 * 60 * 60 * repoUpdateInHours);
+http.createServer(app).listen(app.get('port'), function() {
+  // We are live!
+  console.log('Main Server listening on port ' + app.get('port'));
+  // One time tasks for new server boot
+  runStartupTasks();
+  // Reoccuring tasks
+  setInterval(function() {
+    global.bitbucket_repos = [];
+    global.github_repos = [];
+    getGithubReposInfo();
+    getBitbucketReposInfo();
+    global.repos_updated_last = new moment();
+  }, 1000 * 60 * 60 * repoUpdateInHours);
 });
 
 function runStartupTasks() {
-	global.bitbucket_repos = [];
-	global.github_repos = [];
-	global.repos_updated_last = new moment();
-	getGithubReposInfo();
-	getBitbucketReposInfo();
+  global.bitbucket_repos = [];
+  global.github_repos = [];
+  global.repos_updated_last = new moment();
+  getGithubReposInfo();
+  getBitbucketReposInfo();
 }
 
 function getBitbucketReposInfo() {
-	// Go get newest bitbucket info for Sarophym account
-	getBitbucketPage(1);
+  // Go get newest bitbucket info for Sarophym account
+  getBitbucketPage(1);
 }
 
 function getBitbucketPage(page) {
-	var opts = {
-		host: "bitbucket.org",
-		method: 'GET',
-		path: "/api/2.0/repositories/sarophym?page=" + page,
-		headers: {
-			Authorization: bitbucketAuth
-		}
-	};
+  var opts = {
+    host: "bitbucket.org",
+    method: 'GET',
+    path: "/api/2.0/repositories/sarophym?page=" + page,
+    headers: {
+      Authorization: bitbucketAuth
+    }
+  };
 
-	var bitbucketRequest = https.request(opts, function (res) {
-		var totalMessage = '';
+  var bitbucketRequest = https.request(opts, function(res) {
+    var totalMessage = '';
 
-		res.on('data', (chunk) => {
-			totalMessage += chunk;
-		}).on('end', () => {
-			var body = JSON.parse(totalMessage);
-			global.bitbucket_repos = global.bitbucket_repos || [];
-			global.bitbucket_repos = global.bitbucket_repos.concat(body.values);
+    res.on('data', (chunk) => {
+      totalMessage += chunk;
+    }).on('end', () => {
+      var body = JSON.parse(totalMessage);
+      global.bitbucket_repos = global.bitbucket_repos || [];
+      global.bitbucket_repos = global.bitbucket_repos.concat(body.values);
 
-			if (body.next !== undefined) {
-				getBitbucketPage(++page);
-			}
-		});
-	}).on('error', function (e) {
-		console.log("Bitbucket errored: " + e.message);
-	});
+      if (body.next !== undefined) {
+        getBitbucketPage(++page);
+      }
+    });
+  }).on('error', function(e) {
+    console.log("Bitbucket errored: " + e.message);
+  });
 
-	bitbucketRequest.end();
+  bitbucketRequest.end();
 }
 
 function getGithubReposInfo() {
-	var opts = {
-		host: "api.github.com",
-		method: 'GET',
-		path: "/users/butlersrepos/repos",
-		headers: {
-			'Accept': 'application/vnd.github.v3+json',
-			'User-Agent': 'butlersrepos'
-		}
-	};
+  var opts = {
+    host: "api.github.com",
+    method: 'GET',
+    path: "/users/butlersrepos/repos",
+    headers: {
+      'Accept': 'application/vnd.github.v3+json',
+      'User-Agent': 'butlersrepos'
+    }
+  };
 
-	var githubRequest = https.request(opts, function (res) {
-		var totalMessage = '';
+  var githubRequest = https.request(opts, function(res) {
+    var totalMessage = '';
 
-		res.on('data', (chunk) => {
-			totalMessage += chunk;
-		}).on('end', () => {
-			global.github_repos = JSON.parse(totalMessage);
-		})
-	}).on('error', function (e) {
-		console.log("Github errored: " + e.message);
-	});
+    res.on('data', (chunk) => {
+      totalMessage += chunk;
+    }).on('end', () => {
+      global.github_repos = JSON.parse(totalMessage);
+    })
+  }).on('error', function(e) {
+    console.log("Github errored: " + e.message);
+  });
 
-	githubRequest.end();
+  githubRequest.end();
 }
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,55 @@
 // Moment plugin for time and date parsing
-var moment = require( 'moment' );
-var resumeJson = require( './my-resume' );
-var express = require( 'express' );
+var moment = require('moment');
+var resumeJson = require('../src/my-resume');
+var express = require('express');
 var router = express.Router();
 
 // Main Start Page
-function mainPage(req, res ) {
-	console.log( 'Starting!' );
-	res.render( 'index.jade' );
-	console.log( req.host )
+function mainPage(req, res) {
+  console.log('Starting!');
+  res.render('index.jade');
+  console.log(req.host)
 }
 
 function homePage(req, res) {
-	console.log( 'Starting!' );
-	res.render( 'content/home.jade' );
+  console.log('Starting!');
+  res.render('content/home.jade');
 }
 
 // A Short AJAXed Autobiography
-function aboutMePage( req, res ) {
-	console.log( 'Loading AboutMe...' );
-	res.render( 'content/aboutme.jade' );
+function aboutMePage(req, res) {
+  console.log('Loading AboutMe...');
+  res.render('content/aboutme.jade');
 }
 
 // An AJAXed Resume
-function resumePage( req, res ) {
-	console.log( 'Loading Resume...' );
-	res.render( 'content/resume.jade', { resume: resumeJson } );
+function resumePage(req, res) {
+  console.log('Loading Resume...');
+  res.render('content/resume.jade', {resume: resumeJson});
 }
 
 // RESTful API Stuff
-function getJsonResume( req, res ) {
-	res.writeHead( 200, { "Content-Type": "application/json" } );
-	res.end( JSON.stringify( resumeJson ) );
+function getJsonResume(req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(JSON.stringify(resumeJson));
 }
 
 // List of github repos etc
-function worksPage( req, res ) {
-	res.render( 'content/works.jade', {
-		moment         : moment,
-		updated_last   : global.repos_updated_last,
-		github_repos   : global.github_repos,
-		bitbucket_repos: global.bitbucket_repos
-	} );
+function worksPage(req, res) {
+  res.render('content/works.jade', {
+    moment: moment,
+    updated_last: global.repos_updated_last,
+    github_repos: global.github_repos,
+    bitbucket_repos: global.bitbucket_repos
+  });
 }
 
-router.get( '/', mainPage );
-router.get( '/home', homePage );
-router.get( '/aboutme', aboutMePage );
-router.get( '/resume', resumePage );
-router.get( '/works', worksPage );
-router.get( '/api/resume/get', getJsonResume );
+router.get('/', mainPage);
+router.get('/home', homePage);
+router.get('/aboutme', aboutMePage);
+router.get('/resume', resumePage);
+router.get('/works', worksPage);
+router.get('/api/resume/get', getJsonResume);
 
 // Exposed pieces
 module.exports = router;
