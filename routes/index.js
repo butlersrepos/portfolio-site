34144aa201a907059238c9f066e134c2a976e1f3
Eric M Butler <ericbutler1@Erics-MacBook-Pro-3.local>
2017-02-19 22:17:06 -0500

    Added link to civic hacks game

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', mainPage);
-router.get('/home', homePage);
-router.get('/aboutme', aboutMePage);
-router.get('/resume', resumePage);
-router.get('/works', worksPage);
 
 router.get('/api/resume/get', getJsonResume);
 router.get('/api/githubs', getGithubs);
@@ -27,37 +23,10 @@ function mainPage(req, res) {
   console.log(req.host)
 }
 
-function homePage(req, res) {
-  console.log('Starting!');
-  res.render('content/home.jade');
-}
-
-// A Short AJAXed Autobiography
-function aboutMePage(req, res) {
-  console.log('Loading AboutMe...');
-  res.render('content/aboutme.jade');
-}
-
-// An AJAXed Resume
-function resumePage(req, res) {
-  console.log('Loading Resume...');
-  res.render('content/resume.jade', {resume: resumeJson});
-}
-
 // RESTful API Stuff
 function getJsonResume(req, res) {
   res.writeHead(200, {"Content-Type": "application/json"});
   res.end(JSON.stringify(resumeJson));
-}
-
-// List of github repos etc
-function worksPage(req, res) {
-  res.render('content/works.jade', {
-    moment: moment,
-    updated_last: global.repos_updated_last,
-    github_repos: global.github_repos,
-    bitbucket_repos: global.bitbucket_repos
-  });
 }
 
 function getGithubs(req, res) {
