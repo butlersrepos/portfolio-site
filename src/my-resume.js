56722077411cefbd4296ae33376bb8f58a879d69
Eric Butler <ericbutler000@gmail.com>
2017-12-17 00:21:12 -0800

    Updated resume made link color less loud

--- src/my-resume.js
+++ src/my-resume.js
@@ -1,43 +1,42 @@
 module.exports = {
-	"firstname"  : "Eric",
-	"lastname"   : "Butler",
+	"firstname": "Eric",
+	"lastname": "Butler",
 	"linkedin_id": "ericmbutler",
-	"bio_data"   : {
-		"location"    : "Columbus, OH",
-		"email"       : "ericbutler000@gmail.com",
-		"phone"       : "419.366.9274",
-		"website"     : "ericbutler.info",
-		"skills"      : {
-			"languages"       : [
+	"bio_data": {
+		"location": "San Francisco, CA",
+		"email": "ericbutler000@gmail.com",
+		"phone": "419.366.9274",
+		"website": "ericbutler.info",
+		"skills": {
+			"languages": [
 				"Java 1.5+",
 				"Javascript Lover"
 			],
 			"web technologies": [
-				"HTML5 & CSS3",
+				"HTML & CSS",
 				"ES6",
-				"SASS & LESS",
-				"Angular",
+				"SASS & Styled Components",
+				"Angular 1",
 				"RiotJS",
 				"React",
 				"Jest",
-				"Web Components",
 				"All the build tools"
 			],
-			"versioning"      : [
+			"versioning": [
 				"Git",
 				"SVN"
 			],
-			"IDEs"            : [
+			"IDEs": [
 				"Eclipse",
 				"IntelliJ",
 				"VS Code"
 			],
-			"methodologies"   : [
+			"methodologies": [
 				"agile",
 				"whatever works for the team"
 			]
 		},
-		"hobbies"   : [
+		"hobbies": [
 			"Mtg",
 			"PC Games",
 			"Weight-Lifting",
@@ -49,191 +48,199 @@ module.exports = {
 			"lastname": "Quelette"
 		},
 		"pets": [
-			{ 
-				"species": "rabbit",
-				"name": "digby"
-			},
-			{ 
-				"species": "rabbit",
-				"name": "mocha"
-			},
-			{ 
+			{
 				"species": "rat",
 				"name": "butters"
 			},
-			{ 
+			{
 				"species": "rat",
 				"name": "velcro"
 			},
-			{ 
+			{
 				"species": "cat",
 				"name": "wally"
 			},
-			{ 
+			{
 				"species": "dog",
 				"name": "romy"
 			}
 		],
-		"education" : {
+		"education": {
 			"schools": [
 				{
-					"degree"     : "Master of Arts",
-					"major"      : "Economics",
+					"degree": "Master of Arts",
+					"major": "Economics",
 					"institution": "BGSU",
-					"location"   : "Bowling Green, OH",
-					"grad_month" : "Aug",
-					"grad_year"  : 2009
+					"location": "Bowling Green, OH",
+					"grad_month": "Aug",
+					"grad_year": 2009
 				},
 				{
-					"degree"     : "Bachelor of Arts",
-					"major"      : "Philosophy & Economics",
+					"degree": "Bachelor of Arts",
+					"major": "Philosophy & Economics",
 					"institution": "BGSU",
-					"location"   : "Bowling Green, OH",
-					"grad_year"  : 2007,
-					"grad_month" : "Dec"
+					"location": "Bowling Green, OH",
+					"grad_year": 2007,
+					"grad_month": "Dec"
 				}
 			]
 		},
 		"experience": {
 			"positions": [
 				{
-					"title"   : "Software Craftsman",
+					"title": "Senior Frontend Developer",
+					"employer": "Atlassian",
+					"link": "http://atlassian.com",
+					"start": "September 2017",
+					"end": "Present"
+				},
+				{
+					"title": "Software Craftsman",
 					"employer": "Pillar Technology",
-					"link"    : "http://pillartechnology.com",
-					"start"   : "March 2015",
-					"end"     : "Present"
+					"link": "http://pillartechnology.com",
+					"start": "March 2015",
+					"end": "August 2017"
 				},
 				{
-					"title"   : "Solutions Architect",
+					"title": "Solutions Architect",
 					"employer": "Leading EDJE",
-					"link"    : "http://leadingedje.com/",
-					"start"   : "July 2013",
-					"end"     : "February 2014"
+					"link": "http://leadingedje.com/",
+					"start": "July 2013",
+					"end": "February 2014"
 				},
 				{
-					"title"   : "Senior Consultant",
+					"title": "Senior Consultant",
 					"employer": "Sogeti USA",
-					"link"    : "http://www.us.sogeti.com/",
-					"start"   : "February 2011",
-					"end"     : "June 2013"
+					"link": "http://www.us.sogeti.com/",
+					"start": "February 2011",
+					"end": "June 2013"
 				}
 			],
-			"projects" : [
+			"projects": [
+				{
+					"client": "Bitbucket",
+					"client_link": "https://bitbucket.org",
+					"location": "San Francisco, CA",
+					"start": "September 2017",
+					"end": "Present",
+					"project": "Code Review",
+					"description": "Separating the code review process on Bitbucket from the main application into a SPA using a modern React tech stack."
+				},
 				{
 					"client": "OhioHealth",
 					"client_link": "https://www.ohiohealth.com/",
-					"location"   : "Columbus, OH",
-					"start"      : "April 2017",
-					"end"        : "Present",
-					"project"    : "Main website",
+					"location": "Columbus, OH",
+					"start": "April 2017",
+					"end": "August 2017",
+					"project": "Main website",
 					"description": "Taking over ownership of the homepage for OhioHealth.com, serving as Tech Lead, teaching fellow developers React & Redux, incorporating modern frontend design patterns into a legacy web application."
 				},
 				{
 					"client": "OhioHealth",
 					"client_link": "https://www.ohiohealth.com/",
-					"location"   : "Columbus, OH",
-					"start"      : "May 2016",
-					"end"        : "Present",
-					"project"    : "Hybrid Mobile App",
+					"location": "Columbus, OH",
+					"start": "May 2016",
+					"end": "Present",
+					"project": "Hybrid Mobile App",
 					"description": "Brand new iOS and Android application for client care, doctor finding, bill pay, appointments, etc. Built on top of apache cordova with HTML5-spec custom components. Played the role of SME, Tech Lead, and educator in the javascript space. Worked closely with design, product owner, and multiple 3rd parties."
 				},
 				{
 					"client": "The GAP",
 					"client_link": "http://www.gap.com",
-					"location"   : "Columbus, OH",
-					"start"      : "October 2015",
-					"end"        : "May 2016",
-					"project"    : "Ecommerce",
+					"location": "Columbus, OH",
+					"start": "October 2015",
+					"end": "May 2016",
+					"project": "Ecommerce",
 					"description": "Legacy technology stack of Struts, Spring, JAWR, Grunt, Gulp, JSPs, Angular, DWR. This project was a ultra-long-lived mish-mash of design patterns and glue."
 				},
 				{
-					"client"     : "The GAP",
+					"client": "The GAP",
 					"client_link": "http://www.gap.com",
-					"location"   : "Columbus, OH",
-					"start"      : "March 2015",
-					"end"        : "October 2015",
-					"project"    : "LAS",
+					"location": "Columbus, OH",
+					"start": "March 2015",
+					"end": "October 2015",
+					"project": "LAS",
 					"description": "Modern stack, test driven, agile structured project to store old information in a new way and produce multiple consumable endpoints for other internal applications. Java 8, MongoDB, Spring Data, Angular 1.4."
 				},
 				{
-					"client"     : "DSW",
+					"client": "DSW",
 					"client_link": "http://www.dsw.com",
-					"location"   : "Columbus, OH",
-					"start"      : "April 2014",
-					"end"        : "Present",
-					"project"    : "Ecommerce Frontend Team",
+					"location": "Columbus, OH",
+					"start": "April 2014",
+					"end": "Present",
+					"project": "Ecommerce Frontend Team",
 					"description": "This project involved Git, Java, ATG 9.4, ATG 11, Endeca, Bamboo, Eclipse, IntelliJ, some batch scripting, and a multitude of other pieces as I helped move them into an upgraded environment and redesign of their entire retail website. This team was also fairly new to Agile and sought advice in that respect. I also helped establish coding standards, spread git usage knowledge to many first-time users, and setup design patterns for Spring MVC and frontend code."
 				},
 				{
-					"client"     : "Crown Equipment",
+					"client": "Crown Equipment",
 					"client_link": "http://www.crown.com/usa/index.html",
-					"location"   : "New Bremen, OH",
-					"start"      : "July 2013",
-					"end"        : "April 2014",
-					"project"    : "Module Simulation",
+					"location": "New Bremen, OH",
+					"start": "July 2013",
+					"end": "April 2014",
+					"project": "Module Simulation",
 					"description": "Along with a change in employer this project took me in a fresh direction from my status quo forcing me to acquire more knowledge of low-level operations in a Java environment, AWS Cloud management, and network communications. This project also used Agile methodology in a remote team setting."
 				},
 				{
-					"client"     : "Nationwide",
+					"client": "Nationwide",
 					"client_link": "http://www.nationwide.com/investments-and-retirement.jsp",
-					"location"   : "Columbus, OH",
-					"start"      : "February 2013",
-					"end"        : "June 2013",
-					"project"    : "Investments Team App Support",
+					"location": "Columbus, OH",
+					"start": "February 2013",
+					"end": "June 2013",
+					"project": "Investments Team App Support",
 					"description": "My involvement in this project required me to familiarize myself with numerous web apps that the team already supported across various platforms. I became acclimated to debugging and improving processes in C#, Informatica, SQL, and Maestro as well as my wheelhouse Java. This team was deeply invested in the Agile methodology."
 				},
 				{
-					"client"     : "Safelite",
+					"client": "Safelite",
 					"client_link": "http://www.safelite.com/",
-					"location"   : "Columbus, OH",
-					"start"      : "July 2012",
-					"end"        : "January 2013",
-					"project"    : "Service Auto Glass",
+					"location": "Columbus, OH",
+					"start": "July 2012",
+					"end": "January 2013",
+					"project": "Service Auto Glass",
 					"description": "This project was a complete overhaul of the wholesalers\" sales interface to Service Auto Glass. I had also spear-headed the entire mobilization of the web site; demoing a proof of concept and determining the architecture and technologies used. This project was built on Spring 3.0, jQuery 1.7, Maven 3, and Tomcat 7."
 				},
 				{
-					"client"     : "Abercrombie & Fitch",
+					"client": "Abercrombie & Fitch",
 					"client_link": "http://www.abercrombie.com/shop/us",
-					"location"   : "Columbus, OH",
-					"start"      : "April 2012",
-					"end"        : "June 2012",
-					"project"    : "Outbound WMS",
+					"location": "Columbus, OH",
+					"start": "April 2012",
+					"end": "June 2012",
+					"project": "Outbound WMS",
 					"description": "This was about adding new functionality and improving old functionality on the Warehouse Management Systems. The project involved significant Java work within a Struts and Spring framework and overhauling older design pattern for newer ones friendly to jQuery, CSS3, HTML5, and future Spring integrations. Other tools used: Apache Tomcat, Weblogic, and AJAX."
 				},
 				{
-					"client"     : "Abercrombie & Fitch",
+					"client": "Abercrombie & Fitch",
 					"client_link": "http://www.abercrombie.com/shop/us",
-					"location"   : "Columbus, OH",
-					"start"      : "January 2012",
-					"end"        : "April 2012",
-					"project"    : "Visual Merchandising Systems",
+					"location": "Columbus, OH",
+					"start": "January 2012",
+					"end": "April 2012",
+					"project": "Visual Merchandising Systems",
 					"description": "This project encompassed an extensive frontend goal which involved thousands of lines of customized javascript and rewritten jQuery extensions. I was specifically requested on this team after my exposure to their apps while on Store Systems. Technologies consisted of Java, Struts, javascript, jQuery, HTML, CSS3, Jahia, Maven, and AJAX."
 				},
 				{
-					"client"     : "Abercrombie & Fitch",
+					"client": "Abercrombie & Fitch",
 					"client_link": "http://www.abercrombie.com/shop/us",
-					"location"   : "Columbus, OH",
-					"start"      : "October 2011",
-					"end"        : "January 2012",
-					"project"    : "Global Trade Database",
+					"location": "Columbus, OH",
+					"start": "October 2011",
+					"end": "January 2012",
+					"project": "Global Trade Database",
 					"description": "I was placed onto this team to speed up development of a project behind schedule. Utilized GWT, Java, WebLogic, AJAX, and Oracle SQL. Significant overtime was necessary to finish on schedule. I learned valuable lessons such as; don\"t use GWT."
 				},
 				{
-					"client"     : "Abercrombie & Fitch",
+					"client": "Abercrombie & Fitch",
 					"client_link": "http://www.abercrombie.com/shop/us",
-					"location"   : "Columbus, OH",
-					"start"      : "May 2011",
-					"end"        : "October 2011",
-					"project"    : "Store Systems",
+					"location": "Columbus, OH",
+					"start": "May 2011",
+					"end": "October 2011",
+					"project": "Store Systems",
 					"description": "I contributed development to store contracts systems and interfaces. I owned the design and development of a java web-app, with spare time from main projects. Technologies involved full back to front development: Java backend, Spring 3.0, Maven, Apache Tomcat, CSS, HTML, jQuery, AJAX, javascript, and Oracle SQL."
 				},
 				{
-					"client"     : "Abercrombie & Fitch",
+					"client": "Abercrombie & Fitch",
 					"client_link": "http://www.abercrombie.com/shop/us",
-					"location"   : "Columbus, OH",
-					"start"      : "March 2011",
-					"end"        : "May 2011",
-					"project"    : "Ecommerce Frontend",
+					"location": "Columbus, OH",
+					"start": "March 2011",
+					"end": "May 2011",
+					"project": "Ecommerce Frontend",
 					"description": "Here I updated and maintained the primary commercial websites of Abercrombie & Fitch, Abercrombie kids, Hollister, and Gilly Hicks. Tasks utilized CSS 3, HTML, JSPs, SVN, Apache Tomcat, and WebSphere. I also implemented a Java based automation of repetitive content management tasks."
 				}
 			]
